268526151cc17f1deb41f3b307a98c6a06780c9a
Muhammad Salman <salmandotweb@gmail.com>
2022-08-08 03:35:45 +0500

    updated

--- frontend/src/components/home/PostCard.js
+++ frontend/src/components/home/PostCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { HiOutlineVideoCamera, HiOutlinePhotograph } from "react-icons/hi";
+import { BsEmojiSmile } from "react-icons/bs";
 import classes from "../../styles/stylesheets/PostCard.module.css";
 
 const PostCard = () => {
@@ -17,6 +19,21 @@ const PostCard = () => {
 					placeholder={`What's happening ${user?.firstName}?`}
 					className={`input ${classes.postInput}`}
 				/>
+			</div>
+			<div className={classes.options}>
+				<div className={classes.optionBtns}>
+					<button className={`btn ${classes.optionBtn}`}>
+						<HiOutlineVideoCamera /> Live Video
+					</button>
+					<button className={`btn ${classes.optionBtn}`}>
+						<HiOutlinePhotograph /> Photo/Video
+					</button>
+					<button className={`btn ${classes.optionBtn}`}>
+						<BsEmojiSmile />
+						Feeling
+					</button>
+				</div>
+				<button className="btn">Post</button>
 			</div>
 		</div>
 	);
